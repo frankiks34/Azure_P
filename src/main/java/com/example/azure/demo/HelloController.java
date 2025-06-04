ce21ca8999711b2e0ba7a744c325f8ee22956062
Parquefc Rondon tinoco <u23259579@utp.edu.pe>
2025-06-03 20:45:58 -0500

    avance1

---
+++ src/main/java/com/example/azure/demo/HelloController.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.azure.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "¡Hola te saludo desde azure!";
+    }
+}
